16d804479445447505834fa15336b4cda2fd0b71
RHEZUS <ludivinseunda2@gmail.com>
2024-03-20 09:50:44 +0300

    a

--- console.py
+++ console.py
@@ -4,7 +4,6 @@ import cmd
 import sys
 from models import storage
 from models.base_model import BaseModel
-from models.__init__ import storage
 from models.user import User
 from models.place import Place
 from models.state import State
