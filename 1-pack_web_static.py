ba9a0e178cf1e9da74122fe2848e88eb32526ebf
RHEZUS <ludivinseunda2@gmail.com>
2024-04-05 10:47:23 +0300

    make task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,10 +5,12 @@ folder of the AirBnB Clone repo
 """
 
 from datetime import datetime
+from fabric import task
 from fabric.api import local
 from os.path import isdir
 
 
+@task
 def do_pack():
     """generates a tgz archive"""
     try:
